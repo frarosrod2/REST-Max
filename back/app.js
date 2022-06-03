57fbac5a4653f45951844af819bce330a521bba0
Francisco Javier Rosa Rodríguez <pacorosa99@gmail.com>
2022-06-03 09:15:42 +0200

    Update app.js

--- back/app.js
+++ back/app.js
@@ -55,7 +55,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    'mongodb+srv://admin:admin@cluster0.x6vsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    'mongodb+srv://admin:<password>@cluster0.x6vsw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   )
   .then((result) => {
     const server = app.listen(8080);
